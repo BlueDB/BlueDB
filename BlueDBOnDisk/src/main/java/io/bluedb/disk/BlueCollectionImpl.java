570ef8c0463a4bc76dc7fa884885d8d92424afa4
Jeremy <jlakey@ximasoftware.com>
2018-06-13 09:58:08 -0600

    interface improvements

--- BlueDBOnDisk/src/main/java/io/bluedb/disk/BlueCollectionImpl.java
+++ BlueDBOnDisk/src/main/java/io/bluedb/disk/BlueCollectionImpl.java
@@ -19,7 +19,7 @@ public class BlueCollectionImpl<T extends Serializable> implements BlueCollectio
 	}
 
 	@Override
-	public void insert(T object, BlueKey key) throws BlueDbException {
+	public void insert(BlueKey key, T object) throws BlueDbException {
 		// TODO Auto-generated method stub
 
 	}
