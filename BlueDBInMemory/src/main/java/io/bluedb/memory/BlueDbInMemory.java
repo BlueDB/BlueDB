51e6b66d9ada0ddbeb292c447c7337bee3638008
Jeremy <jeremy.lakey@gmail.com>
2018-06-14 10:05:55 -0600

    fix synchronization and syntax issues

--- BlueDBInMemory/src/main/java/io/bluedb/memory/BlueDbInMemory.java
+++ BlueDBInMemory/src/main/java/io/bluedb/memory/BlueDbInMemory.java
@@ -9,16 +9,17 @@ import io.bluedb.api.BlueCollection;
 
 public class BlueDbInMemory implements BlueDb {
 
-	@SuppressWarnings("rawtypes")
-	Map<Class, BlueCollection> collections = new HashMap<>();
+	Map<Class<?>, BlueCollection<?>> collections = new HashMap<>();
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends Serializable> BlueCollection<T> getCollection(Class<T> type) {
-		if (!collections.containsKey(type)) {
-			collections.put(type, new BlueCollectionImpl<T>(type));
+		synchronized(collections) {
+			if (!collections.containsKey(type)) {
+				collections.put(type, new BlueCollectionImpl<T>(type));
+			}
+			return (BlueCollection<T>)(collections.get(type));
 		}
-		return (BlueCollection<T>)(collections.get(type));
 	}
 
 	@Override
