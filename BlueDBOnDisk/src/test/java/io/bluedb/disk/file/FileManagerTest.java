6d9aa78f741752a6f5e4750101d84bff9f69f88b
Jeremy <jeremy.lakey@gmail.com>
2018-06-26 09:10:46 -0600

    create failing multi-threaded test

--- BlueDBOnDisk/src/test/java/io/bluedb/disk/file/FileManagerTest.java
+++ BlueDBOnDisk/src/test/java/io/bluedb/disk/file/FileManagerTest.java
@@ -3,9 +3,12 @@ package io.bluedb.disk.file;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
 
@@ -41,7 +44,7 @@ public class FileManagerTest  extends TestCase {
 		File nonEmptyFolder = new File("owa_tana_siam");
 		nonEmptyFolder.mkdirs();
 		File fileWithSuffix = createFile(nonEmptyFolder, "legit" + suffix);
-		File junk = createFile(nonEmptyFolder, "junk");
+		createFile(nonEmptyFolder, "junk");
 		List<File> filesWithSuffix = fileManager.listFiles(nonEmptyFolder.toPath(), suffix);
 		assertEquals(1, filesWithSuffix.size());
 		assertTrue(filesWithSuffix.contains(fileWithSuffix));
@@ -98,7 +101,46 @@ public class FileManagerTest  extends TestCase {
 
 		recursiveDelete(fileWithNull);
 		recursiveDelete(fileWithValue);
-}
+	}
+
+	@Test
+	public void test_multithreaded() {
+		TestValue value = new TestValue("joe", 0);
+		File file = createFileAndWriteTestValue("testMultithreaded", value);
+		Path path = file.toPath();
+
+		int attemptsToOpen = 500;
+		CountDownLatch doneSignal = new CountDownLatch(attemptsToOpen);
+		AtomicLong successfulOpens = new AtomicLong(0);
+
+		List<Thread> threads = new ArrayList<>();
+		for (int i = 0; i < attemptsToOpen; i++) {
+			Runnable reader = new Runnable() {
+				@Override public void run() {
+					TestValue loadedValue = loadTestValueFromPathOrNull(path);
+					if (loadedValue != null) {
+						successfulOpens.incrementAndGet();
+					}
+					doneSignal.countDown();
+				}
+			};
+			Thread readerThread = new Thread(reader);
+			threads.add(readerThread);
+		}
+		
+		// interleave writing and reading
+		while(doneSignal.getCount() > 0) {
+			if (!threads.isEmpty()) {
+				Thread thread = threads.remove(threads.size()-1);
+				thread.start();
+			}
+			loadAddCupcakeAndSave(path);
+		}
+
+		assertEquals(attemptsToOpen, successfulOpens.get());
+
+		recursiveDelete(file);
+	}
 
 	private void recursiveDelete(File file) {
 		if (!file.exists()) {
@@ -147,4 +189,36 @@ public class FileManagerTest  extends TestCase {
 		}
 		return file;
 	}
+
+	private File createFileAndWriteTestValue(String pathString, TestValue value) {
+		File file = new File(pathString);
+		try {
+			fileManager.saveObject(file.toPath(), value);
+		} catch (BlueDbException e) {
+			e.printStackTrace();
+			fail();
+		}
+		return file;
+	}
+
+	private void loadAddCupcakeAndSave(Path path) {
+		try {
+			TestValue value = (TestValue) fileManager.loadObject(path);
+			value.addCupcake();
+			fileManager.saveObject(path, value);
+		} catch (BlueDbException e) {
+			System.out.println("attempt to save failed");
+		}
+	}
+
+	private TestValue loadTestValueFromPathOrNull(Path path) {
+		try {
+			TestValue value = (TestValue) fileManager.loadObject(path);
+			return value;
+		} catch (BlueDbException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
+
