059ec5a50d8eb7d66d256992e3447f4801b40956
Derek Johnson <djohnson@ximasoftware.com>
2018-06-27 08:42:48 -0600

    Only one collection per class/name pair will be created now.

--- BlueDBOnDisk/src/main/java/io/bluedb/disk/BlueDbOnDisk.java
+++ BlueDBOnDisk/src/main/java/io/bluedb/disk/BlueDbOnDisk.java
@@ -2,6 +2,8 @@ package io.bluedb.disk;
 
 import java.io.Serializable;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.bluedb.api.BlueCollection;
 import io.bluedb.api.BlueDb;
@@ -12,14 +14,24 @@ public class BlueDbOnDisk implements BlueDb {
 
 	private final Path path;
 	
+	private final Map<String, BlueCollection<?>> collections = new HashMap<>();
+	
 	BlueDbOnDisk(Path path, Class<?>...registeredSerializableClasses) {
 		this.path = path;
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public <T extends Serializable> BlueCollection<T> getCollection(Class<T> type, String name) throws BlueDbException {
-		// TODO make sure only one Collection object per collection to avoid concurrency issues
-		return new BlueCollectionImpl<>(this, type);
+		synchronized (collections) {
+			String key = type.getName() + "-" + name;
+			BlueCollection<T> collection = (BlueCollection<T>) collections.get(key);
+			if(collection == null) {
+				collection = new BlueCollectionImpl<>(this, type);
+				collections.put(key, collection);
+			}
+			return collection;
+		}
 	}
 
 	@Override
