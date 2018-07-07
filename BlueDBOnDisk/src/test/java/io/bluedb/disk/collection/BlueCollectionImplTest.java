221ecc382180e1c4b5a22a7f62b3180184b714a6
jeremy <jeremy@jeremy-ThinkPad-T430>
2018-07-07 14:39:54 -0600

    add more tests

--- BlueDBOnDisk/src/test/java/io/bluedb/disk/collection/BlueCollectionImplTest.java
+++ BlueDBOnDisk/src/test/java/io/bluedb/disk/collection/BlueCollectionImplTest.java
@@ -6,11 +6,18 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
 import io.bluedb.api.BlueDb;
+import io.bluedb.api.Condition;
 import io.bluedb.api.exceptions.BlueDbException;
 import io.bluedb.api.keys.BlueKey;
 import io.bluedb.api.keys.StringKey;
@@ -21,6 +28,7 @@ import io.bluedb.disk.TestValue;
 import io.bluedb.disk.segment.Segment;
 import io.bluedb.disk.segment.SegmentManager;
 import io.bluedb.disk.segment.TimeRange;
+import io.bluedb.disk.serialization.BlueEntity;
 import junit.framework.TestCase;
 
 public class BlueCollectionImplTest extends TestCase {
@@ -150,17 +158,58 @@ public class BlueCollectionImplTest extends TestCase {
 
 	@Test
 	public void test_findMatches() {
-		// TODO
-	}
+		TestValue valueJoe = new TestValue("Joe");
+		TestValue valueBob = new TestValue("Bob");
+		insert(1, valueJoe);
+		insert(2, valueBob);
+		List<BlueEntity<TestValue>> allEntities, entitiesWithJoe, entities3to5, entities2to3, entities0to1, entities0to0;
+		try {
+			Condition<TestValue> isJoe = (v) -> v.getName().equals("Joe");
+			allEntities = getCollection().findMatches(0, 3, new ArrayList<>());
+			entitiesWithJoe = getCollection().findMatches(0, 5, Arrays.asList(isJoe));
+			entities3to5 = getCollection().findMatches(3, 5, new ArrayList<>());
+			entities2to3 = getCollection().findMatches(2, 3, new ArrayList<>());
+			entities0to1 = getCollection().findMatches(0, 1, new ArrayList<>());
+			entities0to0 = getCollection().findMatches(0, 0, new ArrayList<>());
 
-	@Test
-	public void test_applyChange() {
-		// TODO
+			assertEquals(2, allEntities.size());
+			assertEquals(1, entitiesWithJoe.size());
+			assertEquals(valueJoe, entitiesWithJoe.get(0).getValue());
+			assertEquals(0, entities3to5.size());
+			assertEquals(1, entities2to3.size());
+			assertEquals(valueBob, entities2to3.get(0).getValue());
+			assertEquals(1, entities0to1.size());
+			assertEquals(valueJoe, entities0to1.get(0).getValue());
+			assertEquals(0, entities0to0.size());
+
+		} catch (BlueDbException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 	@Test
 	public void test_executeTask() {
-		// TODO
+		AtomicBoolean hasRun = new AtomicBoolean(false);
+		Runnable task = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(10);  // make sure this test waits for the task to be complete
+					hasRun.set(true);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					fail();
+				}
+			}
+		};
+		try {
+			getCollection().executeTask(task);
+		} catch (BlueDbException e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertTrue(hasRun.get());
 	}
 
 	@Test
@@ -210,7 +259,35 @@ public class BlueCollectionImplTest extends TestCase {
 
 	@Test
 	public void test_scheduleRollup() {
-		// TODO
+		BlueKey key1At1 = createKey(1, 1);
+		BlueKey key3At3 = createKey(3, 3);
+		TestValue value1 = createValue("Anna");
+		TestValue value3 = createValue("Chuck");
+		List<TestValue> values;
+		try {
+			getCollection().insert(key1At1, value1);
+			getCollection().insert(key3At3, value3);
+			values = getCollection().query().getList();
+			assertEquals(2, values.size());
+			
+			Segment<TestValue> segment = getCollection().getSegmentManager().getSegment(key1At1.getGroupingNumber());
+			File[] segmentDirectoryContents = segment.getPath().toFile().listFiles();
+			assertEquals(2, segmentDirectoryContents.length);
+
+			long segmentSize = SegmentManager.getSegmentSize();
+			TimeRange entireFirstSegmentTimeRange = new TimeRange(0, segmentSize -1);
+			getCollection().scheduleRollup(entireFirstSegmentTimeRange);
+			waitForExecutorToFinish();
+
+			values = getCollection().query().getList();
+			assertEquals(2, values.size());
+			segmentDirectoryContents = segment.getPath().toFile().listFiles();
+			assertEquals(1, segmentDirectoryContents.length);
+
+		} catch (BlueDbException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 	@Test
@@ -292,5 +369,16 @@ public class BlueCollectionImplTest extends TestCase {
 
 	private BlueCollectionImpl<TestValue> getCollection() {
 		return collection;
+	}
+
+	private void waitForExecutorToFinish() {
+		Runnable doNothing = new Runnable() {@Override public void run() {}};
+		Future<?> future = getCollection().executor.submit(doNothing);
+		try {
+			future.get();
+		} catch (ExecutionException | InterruptedException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 }
