ed0fd18bad5b2d16e3b4684ad8eab9a8841fa4a2
Derek Johnson <djohnson@ximasoftware.com>
2019-04-26 16:04:53 -0600

    Fixed TestUtils.getResourcePath even if your IDE gives you a slightly different path.

--- BlueDBOnDisk/src/test/java/org/bluedb/TestUtils.java
+++ BlueDBOnDisk/src/test/java/org/bluedb/TestUtils.java
@@ -1,12 +1,16 @@
 package org.bluedb;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestUtils {
-	public static Path getResourcePath(String relativePath) throws URISyntaxException {
+	public static Path getResourcePath(String relativePath) throws URISyntaxException, IOException {
 		Path pathToStartFrom = Paths.get(TestUtils.class.getResource("").toURI());
-		return pathToStartFrom.resolve("../../../../src/test/resources").resolve(relativePath);
+		while(!pathToStartFrom.endsWith("BlueDBOnDisk")) {
+			pathToStartFrom = pathToStartFrom.resolve("../").toRealPath();
+		}
+		return pathToStartFrom.resolve("src/test/resources").resolve(relativePath);
 	}
 }
