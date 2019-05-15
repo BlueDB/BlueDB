b04cbb6d181568dc8913125ed6a6514d6cd61fcb
Jeremy <jeremy.lakey@gmail.com>
2019-05-15 13:58:42 -0600

    fix typo

--- BlueDBOnDisk/src/test/java/org/bluedb/disk/segment/path/SegmentPathManagerTest.java
+++ BlueDBOnDisk/src/test/java/org/bluedb/disk/segment/path/SegmentPathManagerTest.java
@@ -1,6 +1,6 @@
 package org.bluedb.disk.segment.path;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,16 +18,16 @@ public class SegmentPathManagerTest {
 		assertEquals(Arrays.asList(1L, 524288L), HashSegmentPathManager.DEFAULT_ROLLUP_LEVELS);
 	}
 
-	List<Long> rollupsLong = Arrays.asList(72057594037927936L, 562949953421312L, 2199023255552L, 4294967296L, 8388608L, 16384L, 64L);
-	List<Long> rollupsTime = Arrays.asList(31104000000L, 2592000000L, 86400000L, 3600000L);
-	List<Long> rollupsInt = Arrays.asList(67108864L, 1048576L, 16384L, 256L);
-	List<Long> rollupsHash = Arrays.asList(4294967296L, 67108864L, 524288L);
+	List<Long> foldersLong = Arrays.asList(72057594037927936L, 562949953421312L, 2199023255552L, 4294967296L, 8388608L, 16384L, 64L);
+	List<Long> foldersTime = Arrays.asList(31104000000L, 2592000000L, 86400000L, 3600000L);
+	List<Long> foldersInt = Arrays.asList(67108864L, 1048576L, 16384L, 256L);
+	List<Long> foldersHash = Arrays.asList(4294967296L, 67108864L, 524288L);
 
 	@Test
 	public void test_lock_folder_levels() {
-		assertEquals(rollupsLong, LongSegmentPathManager.DEFAULT_SIZE_FOLDERS);
-		assertEquals(rollupsTime, TimeSegmentPathManager.DEFAULT_SIZE_FOLDERS);
-		assertEquals(rollupsInt, IntegerSegmentPathManager.DEFAULT_SIZE_FOLDERS);
-		assertEquals(rollupsHash, HashSegmentPathManager.DEFAULT_SIZE_FOLDERS);
+		assertEquals(foldersLong, LongSegmentPathManager.DEFAULT_SIZE_FOLDERS);
+		assertEquals(foldersTime, TimeSegmentPathManager.DEFAULT_SIZE_FOLDERS);
+		assertEquals(foldersInt, IntegerSegmentPathManager.DEFAULT_SIZE_FOLDERS);
+		assertEquals(foldersHash, HashSegmentPathManager.DEFAULT_SIZE_FOLDERS);
 	}
 }
