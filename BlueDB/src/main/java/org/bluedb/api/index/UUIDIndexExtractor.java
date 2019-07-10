1ceb0d056ad0ed8b94f9512cb6947ae04c901714
Derek Johnson <djohnson@bluedb.org>
2019-07-10 16:12:09 -0600

    Created functional interfaces for each type of KeyExtractor that are a little easier to use by clients.

---
+++ BlueDB/src/main/java/org/bluedb/api/index/UUIDIndexExtractor.java
@@ -0,0 +1,31 @@
+package org.bluedb.api.index;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.bluedb.api.keys.UUIDKey;
+
+@FunctionalInterface
+public interface UUIDIndexExtractor<V extends Serializable> extends KeyExtractor<UUIDKey, V> {
+	
+	public default Class<UUIDKey> getType() {
+		return UUIDKey.class;
+	}
+	
+	public default List<UUIDKey> extractKeys(V value) {
+		List<UUID> uuids = extractUUIDsForIndex(value);
+		if(uuids != null) {
+			return uuids.stream()
+				.filter(Objects::nonNull)
+				.map(id -> new UUIDKey(id))
+				.collect(Collectors.toCollection(ArrayList::new));
+		}
+		return new ArrayList<>();
+	}
+	
+	public List<UUID> extractUUIDsForIndex(V value);
+}
