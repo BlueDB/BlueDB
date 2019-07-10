70559a8fc55fea79445f48a0c237f412b292691d
Derek Johnson <djohnson@bluedb.org>
2019-07-10 16:38:57 -0600

    Added thorough tests for each of our index and collection types. It creates a backup of the collections to ensure that no future versions break them.

--- BlueDBOnDisk/src/test/java/org/bluedb/TestUtils.java
+++ BlueDBOnDisk/src/test/java/org/bluedb/TestUtils.java
@@ -1,11 +1,19 @@
 package org.bluedb;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.bluedb.api.exceptions.BlueDbException;
+import org.bluedb.api.keys.BlueKey;
+import org.bluedb.api.keys.TimeFrameKey;
+import org.bluedb.api.keys.TimeKey;
+import org.bluedb.disk.IndexableTestValue;
+import org.bluedb.disk.collection.BlueCollectionOnDisk;
 
 public class TestUtils {
 	public static Path getResourcePath(String relativePath) throws URISyntaxException, IOException {
@@ -26,5 +34,92 @@ public class TestUtils {
 		else if(!error.getClass().isAssignableFrom(expected)) {
 			fail(message);
 		}
+	}
+
+	public static void assertCollectionAndValue(BlueCollectionOnDisk<IndexableTestValue> collection, BlueKey key, IndexableTestValue value) throws BlueDbException {
+		assertGet(collection, key, value);
+		assertUpdate(collection, key, value);
+		assertDeleteAndInsert(collection, key, value);
+		assertWhere(collection, key, value);
+		
+		if(key instanceof TimeFrameKey) {
+			assertTimeFrameCases(collection, (TimeFrameKey) key, value);
+		}
+		else if(key instanceof TimeKey) {
+			assertTimeCases(collection, (TimeKey) key, value);
+		}
+	}
+
+	private static void assertGet(BlueCollectionOnDisk<IndexableTestValue> collection, BlueKey key, IndexableTestValue value) throws BlueDbException {
+		assertEquals(value, collection.get(key));
+	}
+
+	private static void assertUpdate(BlueCollectionOnDisk<IndexableTestValue> collection, BlueKey key, IndexableTestValue value) throws BlueDbException {
+		String originalStringValue = value.getStringValue();
+		String newStringValue = "changed";
+		
+		collection.update(key, valueToUpdate -> {
+			valueToUpdate.setStringValue(newStringValue);
+		});
+		
+		assertEquals(newStringValue, collection.get(key).getStringValue());
+		
+		collection.update(key, valueToUpdate -> {
+			valueToUpdate.setStringValue(originalStringValue);
+		});
+		
+		assertEquals(originalStringValue, collection.get(key).getStringValue());
+	}
+
+	private static void assertDeleteAndInsert(BlueCollectionOnDisk<IndexableTestValue> collection, BlueKey key, IndexableTestValue value) throws BlueDbException {
+		assertEquals(true, collection.contains(key));
+		collection.delete(key);
+		assertEquals(false, collection.contains(key));
+		collection.insert(key, value);
+		assertEquals(true, collection.contains(key));
+	}
+
+	private static void assertWhere(BlueCollectionOnDisk<IndexableTestValue> collection, BlueKey key, IndexableTestValue value) throws BlueDbException {
+		assertEquals(true, collection.query().where(iterValue -> value.getId().equals(iterValue.getId())).getList().contains(value));
+	}
+
+	private static void assertTimeCases(BlueCollectionOnDisk<IndexableTestValue> collection, TimeKey key, IndexableTestValue value) throws BlueDbException {
+		assertEquals(false, collection.query().beforeTime(key.getTime()).getList().contains(value));
+		assertEquals(true, collection.query().beforeTime(key.getTime()+1).getList().contains(value));
+		assertEquals(true, collection.query().beforeOrAtTime(key.getTime()).getList().contains(value));
+		
+		assertEquals(false, collection.query().afterTime(key.getTime()).getList().contains(value));
+		assertEquals(true, collection.query().afterTime(key.getTime()-1).getList().contains(value));
+		assertEquals(true, collection.query().afterOrAtTime(key.getTime()).getList().contains(value));
+	}
+
+	private static void assertTimeFrameCases(BlueCollectionOnDisk<IndexableTestValue> collection, TimeFrameKey key, IndexableTestValue value) throws BlueDbException {
+		assertEquals(false, collection.query().beforeTime(key.getStartTime()).getList().contains(value));
+		assertEquals(true, collection.query().beforeTime(key.getStartTime()+1).getList().contains(value));
+		assertEquals(true, collection.query().beforeTime(key.getEndTime()).getList().contains(value));
+		assertEquals(true, collection.query().beforeTime(key.getEndTime()+1).getList().contains(value));
+		assertEquals(true, collection.query().beforeOrAtTime(key.getStartTime()).getList().contains(value));
+		assertEquals(false, collection.query().beforeOrAtTime(key.getStartTime()-1).getList().contains(value));
+		
+		assertEquals(false, collection.query().afterTime(key.getEndTime()).getList().contains(value));
+		assertEquals(true, collection.query().afterTime(key.getEndTime()-1).getList().contains(value));
+		assertEquals(true, collection.query().afterTime(key.getStartTime()).getList().contains(value));
+		assertEquals(true, collection.query().afterTime(key.getStartTime() + 1).getList().contains(value));
+		assertEquals(true, collection.query().afterOrAtTime(key.getEndTime()).getList().contains(value));
+		assertEquals(false, collection.query().afterOrAtTime(key.getEndTime()+1).getList().contains(value));
+	
+		long segmentSize = collection.getSegmentManager().getSegmentSize();
+		boolean overlapsASegment = (key.getStartTime() + segmentSize) < key.getEndTime();
+		
+		assertEquals(overlapsASegment, collection.query().afterTime(key.getStartTime() + segmentSize).getList().contains(value));
+		
+		//By Start Time
+		assertEquals(false, collection.query().byStartTime().beforeTime(key.getStartTime()).getList().contains(value));
+		assertEquals(true, collection.query().byStartTime().beforeTime(key.getStartTime()+1).getList().contains(value));
+		assertEquals(true, collection.query().byStartTime().beforeOrAtTime(key.getStartTime()).getList().contains(value));
+		
+		assertEquals(false, collection.query().byStartTime().afterTime(key.getStartTime()).getList().contains(value));
+		assertEquals(true, collection.query().byStartTime().afterTime(key.getStartTime()-1).getList().contains(value));
+		assertEquals(true, collection.query().byStartTime().afterOrAtTime(key.getStartTime()).getList().contains(value));
 	}
 }
