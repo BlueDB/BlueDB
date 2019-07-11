303d01102dff3a2f50b03a0b0a7c5a8e03455644
Derek Johnson <djohnson@bluedb.org>
2019-07-11 08:17:27 -0600

    Got tests back up to 100% coverage

---
+++ BlueDBOnDisk/src/test/java/org/bluedb/api/index/IntegerIndexExtractorTest.java
@@ -0,0 +1,28 @@
+package org.bluedb.api.index;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.bluedb.api.keys.IntegerKey;
+import org.bluedb.disk.IndexableTestValue;
+import org.junit.Test;
+
+public class IntegerIndexExtractorTest {
+
+	@Test
+	public void test() {
+		IndexableTestValue testValue = new IndexableTestValue(UUID.randomUUID(), 0, 5, "Whatever", 27);
+		
+		IntegerIndexExtractor<IndexableTestValue> extractor = value -> Arrays.asList(value.getIntValue());
+		assertEquals(Arrays.asList(testValue.getIntegerKey()), extractor.extractKeys(testValue));
+		
+		IntegerIndexExtractor<IndexableTestValue> badExtractor = value -> null;
+		assertEquals(Arrays.asList(), badExtractor.extractKeys(testValue));
+		
+		IntegerIndexExtractor<IndexableTestValue> listExtractor = value -> Arrays.asList(value.getIntValue(), 10);
+		assertEquals(Arrays.asList(testValue.getIntegerKey(), new IntegerKey(10)), listExtractor.extractKeys(testValue));
+	}
+
+}
