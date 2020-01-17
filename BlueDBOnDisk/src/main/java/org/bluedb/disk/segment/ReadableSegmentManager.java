a0c6f59b526937ecdea8e97f5aa51ea2dc123c4f
Jeremy <jeremy.lakey@gmail.com>
2020-01-17 08:56:09 -0700

    break SegmentManager into read-only and read-write

--- BlueDBOnDisk/src/main/java/org/bluedb/disk/segment/ReadableSegmentManager.java
+++ BlueDBOnDisk/src/main/java/org/bluedb/disk/segment/ReadableSegmentManager.java
@@ -3,20 +3,25 @@ package org.bluedb.disk.segment;
 import java.io.Serializable;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.bluedb.api.keys.BlueKey;
-import org.bluedb.disk.file.FileManager;
+import org.bluedb.disk.file.ReadFileManager;
 import org.bluedb.disk.segment.path.SegmentPathManager;
 import org.bluedb.disk.segment.path.SegmentSizeConfiguration;
-import org.bluedb.disk.segment.rollup.Rollupable;
 
 public abstract class ReadableSegmentManager<T extends Serializable> {
 
-	private final SegmentPathManager pathManager;
+	protected final SegmentPathManager pathManager;
 
-	public abstract FileManager getFileManager();
-	public abstract Rollupable getRollupable();
+	public abstract ReadFileManager getFileManager();
+	public abstract ReadableSegment<T> getFirstSegment(BlueKey key);
+	public abstract ReadableSegment<T> getSegmentAfter(Segment<T> segment);
+	public abstract ReadableSegment<T> getSegment(long groupingNumber);
+	public abstract List<? extends ReadableSegment<T>> getAllSegments(BlueKey key);
+	public abstract List<? extends ReadableSegment<T>> getAllExistingSegments();
+	public abstract List<? extends ReadableSegment<T>> getExistingSegments(Range range);
+//	protected abstract ReadableSegment<T> toSegment(Path path);
+
 
 	public ReadableSegmentManager(Path collectionPath, SegmentSizeConfiguration sizeConfig) {
 		this.pathManager = createSegmentPathManager(sizeConfig, collectionPath);
@@ -26,46 +31,8 @@ public abstract class ReadableSegmentManager<T extends Serializable> {
 		return Range.forValueAndRangeSize(groupingValue, getSegmentSize());
 	}
 
-	public Segment<T> getFirstSegment(BlueKey key) {
-		long groupingNumber = key.getGroupingNumber();
-		return getSegment(groupingNumber);
-	}
-
-	public Segment<T> getSegmentAfter(Segment<T> segment) {
-		long groupingNumber = segment.getRange().getEnd() + 1;
-		return getSegment(groupingNumber);
-	}
-
-	public Segment<T> getSegment(long groupingNumber) {
-		Path segmentPath = pathManager.getSegmentPath(groupingNumber);
-		return toSegment(segmentPath);
-	}
-
-	public List<Segment<T>> getAllSegments(BlueKey key) {
-		return pathManager.getAllPossibleSegmentPaths(key).stream()
-				.map((p) -> (toSegment(p)))
-				.collect(Collectors.toList());
-	}
-
-	public List<Segment<T>> getAllExistingSegments() {
-		Range allValues = new Range(Long.MIN_VALUE, Long.MAX_VALUE);
-		return getExistingSegments(allValues);
-	}
-
-	public List<Segment<T>> getExistingSegments(Range range) {
-		return pathManager.getExistingSegmentFiles(range).stream()
-				.map((f) -> (toSegment(f.toPath())))
-				.sorted()
-				.collect(Collectors.toList());
-	}
-
 	public SegmentPathManager getPathManager() {
 		return pathManager;
-	}
-
-	protected Segment<T> toSegment(Path path) {
-		Range range = toRange(path);
-		return new Segment<T>(path, range, getRollupable(), getFileManager(), pathManager.getRollupLevels());
 	}
 
 	public Range toRange(Path path) {
