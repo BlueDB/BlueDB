73a2ffefd4710bd7a2d8c4da2aeafe5db50d1d26
Jeremy <jeremy.lakey@gmail.com>
2020-01-17 14:15:53 -0700

    DummyReadOnlyBlueCollectionOnDisk

---
+++ BlueDBOnDisk/src/test/java/org/bluedb/disk/collection/DummyReadOnlyBlueCollectionOnDiskTest.java
@@ -0,0 +1,51 @@
+package org.bluedb.disk.collection;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.bluedb.api.CloseableIterator;
+import org.bluedb.api.ReadBlueTimeQuery;
+import org.bluedb.api.ReadableBlueCollection;
+import org.bluedb.api.ReadableBlueTimeCollection;
+import org.bluedb.api.exceptions.BlueDbException;
+import org.bluedb.api.index.BlueIndex;
+import org.bluedb.api.keys.IntegerKey;
+import org.bluedb.api.keys.ValueKey;
+import org.bluedb.disk.TestValue;
+import org.junit.Test;
+
+public class DummyReadOnlyBlueCollectionOnDiskTest {
+
+	@Test
+	public void test_get() throws BlueDbException {
+		ReadableBlueCollection<TestValue> dummyCollection = new DummyReadOnlyBlueCollectionOnDisk<TestValue>();
+		assertNull(dummyCollection.get(new IntegerKey(1)));
+		assertNull(dummyCollection.getLastKey());
+		assertFalse(dummyCollection.contains(new IntegerKey(1)));
+	}
+
+
+	@Test
+	public void test_index() throws BlueDbException {
+		ReadableBlueCollection<TestValue> dummyCollection = new DummyReadOnlyBlueCollectionOnDisk<TestValue>();
+		BlueIndex<ValueKey, TestValue> dummyIndex = dummyCollection.getIndex("nonexisting index", ValueKey.class);
+		assertNull(dummyIndex.get(new IntegerKey(1)));
+		assertNull(dummyIndex.getLastKey());
+	}
+
+
+	@Test
+	public void test_query() throws BlueDbException, IOException {
+		ReadableBlueTimeCollection<TestValue> dummyCollection = new DummyReadOnlyBlueCollectionOnDisk<TestValue>();
+		ReadBlueTimeQuery<TestValue> dummyQuery = dummyCollection.query().afterOrAtTime(1).beforeOrAtTime(2).where((t) -> true);
+		assertEquals(Arrays.asList(), dummyQuery.getList());
+		assertEquals(0, dummyQuery.count());
+		CloseableIterator<TestValue> dummyIterator = dummyQuery.getIterator();
+		assertFalse(dummyIterator.hasNext());
+		assertEquals(null, dummyIterator.next());
+		assertEquals(null, dummyIterator.peek());
+		dummyIterator.close();
+	}
+}
