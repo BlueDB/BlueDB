888f77406ae723e61c016dbe52013d02f1580463
Derek Johnson <djohnson@bluedb.org>
2020-03-30 13:28:37 -0600

    This commented out code will make it really easy to generate a bluedb jar that prints out all lock usage information just in case we need to troubleshoot another issue like this one.

--- BlueDBOnDisk/src/main/java/org/bluedb/disk/lock/Lock.java
+++ BlueDBOnDisk/src/main/java/org/bluedb/disk/lock/Lock.java
@@ -3,18 +3,29 @@ package org.bluedb.disk.lock;
 import java.util.concurrent.locks.StampedLock;
 
 public class Lock<T> {
-	private StampedLock lock;
+//	private static final AtomicLong NEXT_ID = new AtomicLong(0);
 	
-	public Lock(T key) {
+//	private final long id;
+	private final StampedLock lock;
+//	private final T lockedKey;
+	
+	public Lock(T lockedKey) {
+//		this.id = NEXT_ID.getAndIncrement();
 		this.lock = new StampedLock();
+//		this.lockedKey = lockedKey;
 	}
 
 	public boolean tryToLockForRead() {
-		return lock.asReadLock().tryLock();
+		boolean success = lock.asReadLock().tryLock();
+//		printLockDebugMessage((success ? "Got" : "Failed to get") + " read lock", UUID.randomUUID());
+		return success;
 	}
 
 	public void unlockForRead() {
+//		UUID sessionId = UUID.randomUUID();
+//		printLockDebugMessage("Unlocking read lock", sessionId);
 		lock.asReadLock().unlock();
+//		printLockDebugMessage("Unlocked read lock", sessionId);
 	}
 
 	public boolean isLockedForRead() {
@@ -22,11 +33,17 @@ public class Lock<T> {
 	}
 
 	public void lockForWrite() {
+//		UUID sessionId = UUID.randomUUID();
+//		printLockDebugMessage("Locking write lock", sessionId);
 		lock.asWriteLock().lock();
+//		printLockDebugMessage("Locked write lock", sessionId);
 	}
 
 	public void unlockForWrite() {
+//		UUID sessionId = UUID.randomUUID();
+//		printLockDebugMessage("Unlocking write lock", sessionId);
 		lock.asWriteLock().unlock();
+//		printLockDebugMessage("Unlocked write lock", sessionId);
 	}
 
 	public void lockAndUnlockForWrite() {
@@ -35,4 +52,9 @@ public class Lock<T> {
 		// We need a lock that we know is in the map and that we got the lock before another thread.
 		unlockForWrite();
 	}
+
+//	private void printLockDebugMessage(String message, UUID sessionId) {
+//		String output = "[BlueDbLockDebug] - " + message + " Key: " + lockedKey + "(" + id + ")" + " Thread: " + Thread.currentThread() + " Session: " + sessionId;
+//		Blutils.log(output + System.lineSeparator() + Blutils.getStackTraceAsString());
+//	}
 }
