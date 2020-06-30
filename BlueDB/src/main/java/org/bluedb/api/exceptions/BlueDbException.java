4b9a9d959f73f1448d08fcd18220cde41a3fcb08
Derek Johnson <djohnson@bluedb.org>
2020-06-30 15:54:01 -0600

    Didn't end up using the BlueDbException.from method so I'm removing it

--- BlueDB/src/main/java/org/bluedb/api/exceptions/BlueDbException.java
+++ BlueDB/src/main/java/org/bluedb/api/exceptions/BlueDbException.java
@@ -22,11 +22,4 @@ public class BlueDbException extends Exception {
 	public BlueDbException(String message) {
 		super(message);
 	}
-	
-	public static BlueDbException from(String message, Throwable t) {
-		if(t instanceof BlueDbException) {
-			return (BlueDbException) t;
-		}
-		return new BlueDbException(message, t);
-	}
 }
